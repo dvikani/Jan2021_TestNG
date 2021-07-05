c0804726bc4ce7e56c50534cef8ca192e4cf89ff
dvikani <duhita612@gmail.com>
2021-07-05 11:31:33 -0700

    2nd commit

--- src/main/java/variousConcepts/LearnTestNG.java
+++ src/main/java/variousConcepts/LearnTestNG.java
@@ -1,5 +1,9 @@
 package variousConcepts;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,23 +13,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LearnTestNG {
 
 	WebDriver driver;
-
+	String browser= null;
+	String url;
+	
+	 @BeforeClass
+	public void readConfig() {
+		//BuffereReader//InputStream//FileReader//Scanner---these are the 4 class that can read any kind of file
+		
+		Properties prop = new Properties();   // for java to understand the file system 
+		try {
+			InputStream input = new FileInputStream("./src/main/java/config/config.properties"); 
+			prop.load(input);
+			browser = prop.getProperty("browser");
+			System.out.println("Browser used: " + browser); 
+			url = prop.getProperty("url");
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@BeforeMethod
 	public void inti() {
-		//System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-		//driver = new ChromeDriver();
-		
-		System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
-		driver = new FirefoxDriver();
 		
 		
-		driver.get("https://techfios.com/billing/?ng=login/");
+		if (browser == "chrome") {
+			System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if (browser.equalsIgnoreCase("Firefox") ) {
+			System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		
+		
+		driver.get("url");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -52,7 +81,7 @@ public class LearnTestNG {
 		Assert.assertEquals(DASHBORD_TITLE_ELEMENT.getText(), "Dashboard", "====Wrong Page====");
 	
 	}
-//	@AfterMethod 
+	@AfterMethod 
 	public void tearDown() {
 		driver.close();
 		driver.quit();
